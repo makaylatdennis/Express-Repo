57a63b7f96e9baa37afddbc4b29468610037f83d
Makayla <makaylatd@gmail.com>
2024-06-06 22:15:35 -0400

    updates

--- index.js
+++ index.js
@@ -51,20 +51,31 @@ app.post('/mockData', (req, res) => {
 app.listen(PORT, () => console.log(`Listening on port http://localhost:${PORT}`));
 
 
-app.put('/mockData', (req, res) => {
+app.put('/mockData/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const updatedData = req.body;
-if(!updatedData){
-    return res.status(404).send('Id or name not updated');
-}
-    mockData = mockData.map(item => (item.id === id? updatedData : item));
-    res.json(updatedData)
+
+    if (!updatedData || !updatedData.name) {
+        return res.status(400).send('Must provide valid data to update');
+    }
+
+    const itemIndex = mockData.findIndex(item => item.id === id);
+    if (itemIndex === -1) {
+        return res.status(404).send('Item not found');
+    }
+
+    mockData[itemIndex] = { id, ...updatedData };
+    res.json(mockData[itemIndex]);
 });
 
-app.delete('/mockData', (req, res)=> {
-    const id = id.find(c.id === parseInt(req.params.id));
+app.delete('/mockData/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const itemIndex = mockData.findIndex(item => item.id === id);
 
-   const data = data.indexOf(id);
-    mockData.splice(index, 1);
+    if (itemIndex === -1) {
+        return res.status(404).send('Item not found');
+    }
+
+    mockData.splice(itemIndex, 1);
     res.sendStatus(204);
-})
+});
